2ae7b4af92f56feb4800bd4634059e0f868ea450
vguillemette <vincent.guillemette@dti.ulaval.ca>
2019-12-02 12:59:35 -0500

    * day 1 star 2

--- days/day-1/star-2/solution.js
+++ days/day-1/star-2/solution.js
@@ -1,2 +1,18 @@
+const input = require('./input');
 
-// todo
+const calcFuel = (value, fuelSum = 0) => {
+    const fuel = Math.floor(value/3) - 2;
+    if (fuel < 0) {
+        return fuelSum;
+    } else {
+        return calcFuel(fuel, fuelSum + fuel)
+    }
+};
+
+const solution = input
+    .reduce((acc, cur) => {
+        acc += calcFuel(cur);
+        return acc;
+    }, 0);
+
+console.log(solution);
