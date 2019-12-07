bda995b6347054ba1752f8458cdeb2e34f7f1c0a
unknown <vincentguillemette1981@gmail.com>
2019-12-07 17:06:29 -0500

    * day 4

---
+++ days/day-4/star-1/solution.js
@@ -0,0 +1,19 @@
+const input = require('./input').split('-').map(e => (e.trim(), parseInt(e, 10)));
+
+const candidates = [];
+
+for (let index = input[0]; index <= input[1]; index++) {
+    const asArray = index.toString().split('').map(e => parseInt(e, 10));
+
+    if (
+        asArray.some((e, i, a) => typeof a[i + 1] !== 'undefined' && a[i + 1] === e)
+        &&
+        asArray.every((e, i, a) => typeof a[i + 1] === 'undefined' || a[i + 1] >= e)
+    ) {
+        candidates.push(index);
+    }
+}
+
+const solution = candidates.length;
+
+console.log(solution);
